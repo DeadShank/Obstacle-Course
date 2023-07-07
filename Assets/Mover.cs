46d74c2af04b660a0b0ede4d28596524cbdfa355
DeadShank <rigenkih1996@gmail.com>
2023-07-07 19:59:25 +0300

    first
    first commit

---
+++ Assets/Mover.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float yValue;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal");
+        float zValue = Input.GetAxis("Vertical");
+        transform.Translate(xValue, yValue, zValue);
+    }
+}
