27b96741a5c6ab6c22f80eb1b0dbaaa20525406c
DeadShank <rigenkih1996@gmail.com>
2023-07-10 16:26:05 +0300

    Update Mover.cs

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,23 +5,11 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
-    private void Start()
-    {
-        PrintInstrutcion();
-    }
 
     private void Update()
     {
         movePlayer();
     }
-
-    void PrintInstrutcion()
-    {
-        Debug.Log("Welcome to the game");
-        Debug.Log("String One");
-        Debug.Log("String Two");
-    }
-
     void movePlayer()
     {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
